28c6f5c70a8661cf7b64f61a890f56ffec2e431b
Waleed Almubark <waleed.almubark.dev@gmail.com>
2024-07-28 22:25:48 +0400

    fix checkstyle errors

--- src/main/java/com/example/storediscounts/package-info.java
+++ src/main/java/com/example/storediscounts/package-info.java
@@ -1,5 +1,6 @@
 /**
  * This package is the root package for the store discount application.
- * It contains the main application entry point and common classes for the entire project.
+ * It contains the main application entry point and common classes for the
+ * entire project.
  */
 package com.example.storediscounts;
